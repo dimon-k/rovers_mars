ad390fbdb591d498e0802af84143d89ac01aeb00
Dmitriy Kanununnikov <dmitriy.kanunnikov@gmail.com>
2019-12-21 16:00:50 +0100

    Refactor NasaProgram class and add validations

--- lib/nasa_program.rb
+++ lib/nasa_program.rb
@@ -1,19 +1,19 @@
 class NasaProgram
-  def initialize(*plateau)
-    raise 'Dim to fix to allow rectangular areas!!!' if plateau[0] != plateau[1]
-    raise 'Dim to fix: no more than 2x argumens'     if plateau.count != 2
-    @plateau       = plateau
-    @width         = @plateau[0] + 1
+  def initialize(x_max, y_max)
+    @x_max         = x_max
+    @y_max         = y_max
+    @width         = @x_max + 1
     @compas        = [:N, :E, :S, :W]
     @weights       = { N: @width, E: 1, S:  -@width, W: -1}
-    @working_scale = (@plateau[0] + 1) * (@plateau[1] + 1)
+    @working_scale = (@x_max + 1) * (@y_max + 1)
   end
 
-  def set_initial_position(*coordinates)
-    @coordinates     = coordinates
-    @move_around     = @coordinates[1] * @width + @coordinates[0]
-    @compas_position = @compas.index(@coordinates[2])
-    @current_weight  = @weights[@coordinates[2]]
+  def set_initial_position(x_initial, y_initial, direction)
+    validate_initial_coordinates(x_initial, y_initial, direction)
+
+    @move_around     = y_initial * @width + x_initial
+    @compas_position = @compas.index(direction)
+    @current_weight  = @weights[direction]
   end
 
   def explore(*move_sequence)
@@ -24,12 +24,18 @@ class NasaProgram
 
   private
 
+  def validate_initial_coordinates(x_initial, y_initial, direction)
+    raise 'X is out of range!'  unless x_initial.between?(0, @x_max)
+    raise 'Y is out of range!'  unless y_initial.between?(0, @y_max)
+    raise 'Improper direction!' unless @compas.include?(direction)
+  end
+
   def execute_move(move)
     return turn_left     if move.eql?(:L)
     return turn_right    if move.eql?(:R)
     return move_forward  if move.eql?(:M)
 
-    raise 'Improper value passed!'
+    raise 'Improper move!'
   end
 
   def turn_left
